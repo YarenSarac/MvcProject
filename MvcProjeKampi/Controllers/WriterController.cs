3e67815a7fdda1ef502164f02ac51c7719808cca
YarenSarac <saracyaren9@gmail.com>
2022-02-05 02:01:09 +0300

    Yazar işlemleri

--- MvcProjeKampi/Controllers/WriterController.cs
+++ MvcProjeKampi/Controllers/WriterController.cs
@@ -14,6 +14,8 @@ namespace MvcProjeKampi.Controllers
     public class WriterController : Controller
     {
         WriterManager wm = new WriterManager(new EfWriterDal());
+        WriterValidator writerValidator = new WriterValidator();
+
         public ActionResult Index()
         {
             var WriterValues = wm.GetList();
@@ -27,7 +29,7 @@ namespace MvcProjeKampi.Controllers
         [HttpPost]
         public ActionResult AddWriter(Writer p)
         {
-            WriterValidator writerValidator = new WriterValidator();
+            
             ValidationResult results = writerValidator.Validate(p);
             if (results.IsValid)
             {
@@ -36,12 +38,42 @@ namespace MvcProjeKampi.Controllers
             }
             else
             {
-                foreach(var item in results.Errors)
+                foreach (var item in results.Errors)
                 {
                     ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
                 }
             }
             return View();
         }
+        [HttpGet]
+        public ActionResult EditWriter(int id)
+        {
+            var writervalue = wm.GetByID(id);
+            return View(writervalue);
+
+        }
+        [HttpPost]
+        public ActionResult EditWriter(Writer p)
+        {
+            ValidationResult results = writerValidator.Validate(p);
+            if (results.IsValid)
+            {
+                wm.WriterUpdate(p);
+                return RedirectToAction("Index"); // post ettikten sonra yazarlar sayfasına yönlendiriyor.
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+
+            }
+            return View();
+
+
+
+        }
+
     }
 }
